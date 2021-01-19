ed68d19c61973407b3510d838b8f51468729022a
toannknknk <nguyenkhanhtoan812@gmail.com>
2021-01-19 17:00:04 +0700

    Init Project

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// import Element UI 
+import ElementUI from 'element-ui';
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI);
+
+//import Bootrap
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+//import axios
+import axios from "axios";
+Vue.prototype.$axios = axios
+
 Vue.config.productionTip = false;
 
 new Vue({
